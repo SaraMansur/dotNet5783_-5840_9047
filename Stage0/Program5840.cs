963298807e76612e383872cfabdd55be9bb0cbca
sara <srhm1155@gmail.com>
2022-10-26 11:58:55 +0300

    upload program code

---
+++ Stage0/Program5840.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Xml.Linq;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(String[] args)
+        {
+            Welcome5840();
+            Welcome9047();
+            Console.ReadLine();
+        }
+
+        private static void Welcome5840()
+        {
+            System.Console.WriteLine("Enter Your Name: ");
+            String name = System.Console.ReadLine();
+            System.Console.WriteLine(name + ", welcome to my first console application");
+        }
+        private static void Welcome9047() { }
+
+    }
+}

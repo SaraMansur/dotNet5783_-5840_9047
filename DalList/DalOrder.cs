2c9b85ea65ffcf4a6033a9a7ab30a4fa67ab6819
sara <srhm1155@gmail.com>
2022-11-06 18:55:40 +0200

    tring

---
+++ DalList/DalOrder.cs
@@ -0,0 +1,26 @@
+﻿
+using DO;
+using static Dal.DataSource;
+
+namespace Dal;
+
+
+public class DalOrder
+{
+    public int? Add(Order O)
+    {
+
+        for (int i = 0; i != Config.m_indexEmptyOrder; i++)
+        {
+            if (O.m_ID == DataSource.m_OrderArray[i].m_ID)
+                return null;
+        }
+        DataSource.m_OrderArray[Config.m_indexEmptyOrder++] = O;
+        return O.m_ID;
+    }
+    public void Delete(Product P) { }
+    public void Update(Product P, double price) { }
+    public Order GetbyID(int? ID) { return P.m_ID; }
+
+    ///public IENumerable getArray() { return IEnumerator }
+}

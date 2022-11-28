8b79137e8f0d68cd44f06eff9f80bfa2c0bdb2bf
sara <srhm1155@gmail.com>
2022-11-28 16:00:58 +0200

    שלב 2 אחרוןןןןןןןןןןןןןןןןןןןןן

--- BL/BlImplementation/Product.cs
+++ BL/BlImplementation/Product.cs
@@ -6,7 +6,11 @@ namespace BlImplementation;
 internal class Product: IProduct
 {
     private DalApi.IDal Dal = new Dal.DalList();
-    // the function returns list of products to the manager
+
+    /// <summary>
+    /// the function returns list of products to the manager
+    /// </summary>
+    /// <returns></returns>
     public IEnumerable<BO.ProductForList> ProductList() 
     {
         List<BO.ProductForList> productForLists = new List<BO.ProductForList>();//Going through the list of products
@@ -23,7 +27,10 @@ internal class Product: IProduct
     //    return ProductList();
     //}
 
-    //the function return a catalog for the customer
+    /// <summary>
+    /// the function return a catalog for the customer
+    /// </summary>
+    /// <returns></returns>
     public IEnumerable<BO.ProductItem> CatalogList()
     {
         List<ProductItem> catalogList = new List<ProductItem>();
@@ -39,7 +46,12 @@ internal class Product: IProduct
         return catalogList;
     }
 
-    //the function returns details of the requsted  product to the manager
+    /// <summary>
+    /// the function returns details of the requsted  product to the manager
+    /// </summary>
+    /// <param name="ID"></param>
+    /// <returns></returns>
+    /// <exception cref="FaildGetting"></exception>
     public BO.Product ProductId(int ID)
     {
         if (ID < 0) throw new FaildGetting(new IlegalInput()); //check if the id is correct
@@ -53,7 +65,12 @@ internal class Product: IProduct
         catch (Exception inner) { throw new FaildGetting(inner); }
     }
 
-    //the function returns details of the requsted product for the customer
+    /// <summary>
+    /// the function returns details of the requsted product for the customer
+    /// </summary>
+    /// <param name="ID"></param>
+    /// <returns></returns>
+    /// <exception cref="FaildGetting"></exception>
     public BO.ProductItem CatalogProductId(int ID)
     {
         if (ID < 0) throw new FaildGetting(new IlegalInput());//check if the id is correct
@@ -67,7 +84,11 @@ internal class Product: IProduct
         catch (Exception inner) { throw new FaildGetting(inner); }
     }
 
-    //The function adds a product to the data base
+    /// <summary>
+    /// The function adds a product to the data base
+    /// </summary>
+    /// <param name="product"></param>
+    /// <exception cref="FaildAdding"></exception>
     public void AddProduct(BO.Product product) 
     {
         if (product.m_Id < 0 || product.m_Price < 0 || product.m_InStock < 0 || product.m_Name == "")//check if the data is correct
@@ -77,7 +98,11 @@ internal class Product: IProduct
         catch(Exception inner) { throw new FaildAdding(inner); }   
     }
 
-    //the function deletes the requsted product from the data base
+    /// <summary>
+    /// the function deletes the requsted product from the data base
+    /// </summary>
+    /// <param name="ID"></param>
+    /// <exception cref="FaildDelete"></exception>
     public void DeleteProduct(int ID) 
     {
         bool flag =false;    
@@ -92,7 +117,12 @@ internal class Product: IProduct
         catch (Exception inner ) { throw new FaildDelete(inner); }
     }
 
-    //the function update the requsted product from the data base
+    /// <summary>
+    /// the function update the requsted product from the data base
+    /// </summary>
+    /// <param name="product"></param>
+    /// <exception cref="BO.IlegalInput"></exception>
+    /// <exception cref="FaildUpdating"></exception>
     public void UpdateProduct(BO.Product product) 
     {
         if (product.m_Id < 0 || product.m_Price < 0 || product.m_InStock < 0 || product.m_Name == "")//check if the data is correct

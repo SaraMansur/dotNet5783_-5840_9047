22a33755e9eeff051cabae74e62a5476ea1da759
SaraMansur <smansur@g.jct.ac.il>
2022-12-29 18:14:12 +0200

    שרה

---
+++ PL/WPFOrderTacking/OdrerTacking.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.WPFOrderTacking
+{
+    /// <summary>
+    /// Interaction logic for OdrerTacking.xaml
+    /// </summary>
+    public partial class OdrerTacking : Window
+    {
+        public OdrerTacking()
+        {
+            InitializeComponent();
+        }
+    }
+}

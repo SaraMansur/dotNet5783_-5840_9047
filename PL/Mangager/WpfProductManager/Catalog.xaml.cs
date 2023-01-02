f2857b05132dd56794a46b88f30b2f6e867961f7
OchanaYael <yaochana@g.jct.ac.il>
2023-01-02 16:18:17 +0200

    סינכרון תיקון

--- PL/Mangager/WpfProductManager/Catalog.xaml.cs
+++ PL/Mangager/WpfProductManager/Catalog.xaml.cs
@@ -25,41 +25,33 @@ namespace PL.WpfProduct
     /// </summary>
     public partial class Catalog : Window
     {
-        private BlApi.IBl? bl = BlApi.Factory.Get();
-        private ObservableCollection<ProductForList> productForLists { get; set; }
+        IBl bl = Factory.Get();
+        private ObservableCollection<ProductForList?> _Productlist;
 
         public Catalog()
         {
             InitializeComponent();
-            productForLists = new ObservableCollection<ProductForList>(bl.Product.ProductList().ToList());
-            this.DataContext = productForLists;
+            _Productlist = new(bl.Product.ProductList());
+            this.DataContext = _Productlist;
+
             AttributeSelector.ItemsSource = Enum.GetValues(typeof(BO.Enums.Category));
 
         }
 
-        private void addProduct(ProductForList productForList) => productForLists.Add(productForList);
-        private void Button_Click(object sender, RoutedEventArgs e) => new Changes(addProduct).Show();//opens product window
-
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            _productsList.ItemsSource = bl.Product.FilterBycategory((BO.Enums.Category)AttributeSelector.SelectedItem);
+            productsList.ItemsSource = bl.Product.FilterBycategory((BO.Enums.Category)AttributeSelector.SelectedItem);
         }
 
-        private void AddProduct_Click(object sender, RoutedEventArgs e) { new Changes().Show();  }
+        private void AddProduct_Click(object sender, RoutedEventArgs e) { new Changes().Show(); }
 
         private void MouseDoubleClick_list(object sender, MouseButtonEventArgs e)
         {
             try
             {
-                ProductForList? p1 = (_productsList.SelectedItem as ProductForList);//creats a new productforlist
-                Product product = bl?.Product.ProductId(p1.m_ID);
-                if (product != null)
-                {
-                    //ProductWindow productWindow = new ProductWindow(p1);
-                    //productWindow.ShowDialog();
-                    new Changes(updateProduct, product).ShowDialog();
-
-                }
+                ProductForList pl = (ProductForList)productsList.SelectedItem ?? throw new ArgumentNull();
+                new Changes(pl.m_ID).Show();
+                this.Close();
             }
             catch (Exception ex) { MessageBox.Show(ex.Message.ToString()); }
         }
@@ -68,13 +60,6 @@ namespace PL.WpfProduct
         {
             new MainWindow().Show();
             this.Close();
-        }
-
-        private void updateProduct(ProductForList productForList)
-        {
-            var item = productForLists.FirstOrDefault(x => x.m_ID == productForList.m_ID);
-            int index = productForLists.IndexOf(item);
-            productForLists[index] = productForList;
         }
     }
 }

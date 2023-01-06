4888d165f5827318fea8baadcaa7c5b71ec81988
SaraMansur <smansur@g.jct.ac.il>
2023-01-06 14:24:54 +0200

    התחלת שלב 6

---
+++ DalXml/DalXml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal;
+using DalApi;
+using DO;
+using System.Security.Principal;
+
+sealed  internal class DalXml :IDal
+{
+    public IProduct Product { get; } = new Dal.DalProduct();
+
+    public IOrder Order { get; } = new Dal.Order();
+
+    public IOrderItem OrderItem { get; } = new Dal.DalOrderItem();
+}

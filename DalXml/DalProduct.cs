f4b945b93ff6a780949cc6f4945e09f7a05bf677
OchanaYael <yaochana@g.jct.ac.il>
2023-01-09 19:37:13 +0200

    yuli

--- DalXml/DalProduct.cs
+++ DalXml/DalProduct.cs
@@ -12,12 +12,13 @@ internal class DalProduct : IProduct
 {
     XElement productRoot;
     string productPath = @"Product.xml";
+    string dir = @"..\xml\";
 
     public int P { get; private set; }
 
     public DalProduct()
     {
-        if (!File.Exists(productPath))
+        if (!File.Exists(dir+productPath))
             CreateFiles();
         else
             LoadData();
@@ -25,14 +26,14 @@ internal class DalProduct : IProduct
     private void CreateFiles()
     {
         productRoot = new XElement("Product");
-        productRoot.Save(productPath);
+        productRoot.Save(dir+productPath);
     }
 
     private void LoadData()
     {
         try
         {
-            productRoot = XElement.Load(productPath);
+            productRoot = XElement.Load(dir + productPath);
         }
         catch
         {
@@ -43,29 +44,29 @@ internal class DalProduct : IProduct
     {
         var v = from p in productList
                 select new XElement("Product",
-                    new XElement("ID", p.m_ID),
-                   new XElement("Name", p.m_Name),
-                   new XElement("InStock", p.m_InStock),
-                   new XElement("Price", p.m_Price),
-                   new XElement("Category", p.m_Category)
+                    new XElement("m_ID", p.m_ID),
+                   new XElement("m_Name", p.m_Name),
+                   new XElement("m_InStock", p.m_InStock),
+                   new XElement("m_Price", p.m_Price),
+                   new XElement("m_Category", p.m_Category)
                     );
 
         productRoot = new XElement("Product", v);
 
-        productRoot.Save(productPath);
+        productRoot.Save(dir+productPath);
     }
     public int Add(Product? P)
     {
         var p = Get().FirstOrDefault(x => x?.m_ID == P?.m_ID);
         if (p != null)
             throw new AlreadyExist();
-        XElement ID = new XElement("ID", P?.m_ID);
-        XElement Name = new XElement("Name", P?.m_Name);
-        XElement Category = new XElement("Category", P?.m_Category);
-        XElement InStock = new XElement("InStock", P?.m_InStock);
-        XElement Price = new XElement("Price", P?.m_Price);
+        XElement ID = new XElement("m_ID", P?.m_ID);
+        XElement Name = new XElement("m_Name", P?.m_Name);
+        XElement Category = new XElement("m_Category", P?.m_Category);
+        XElement InStock = new XElement("m_InStock", P?.m_InStock);
+        XElement Price = new XElement("m_Price", P?.m_Price);
         productRoot.Add(new XElement("Product", ID, Name, Category, InStock, Price));
-        productRoot.Save(productPath);
+        productRoot.Save(dir + productPath);
         return (int)P?.m_ID; ;
     }
 
@@ -78,10 +79,10 @@ internal class DalProduct : IProduct
         try
         {
             productElement = (from p in productRoot.Elements()
-                              where Convert.ToInt32(p.Element("ID").Value) == ID
+                              where Convert.ToInt32(p.Element("m_ID").Value) == ID
                               select p).FirstOrDefault();
             productElement.Remove();
-            productRoot.Save(productPath);
+            productRoot.Save(dir + productPath);
             return;
         }
         catch
@@ -94,18 +95,18 @@ internal class DalProduct : IProduct
     {
 
         LoadData();
-        IEnumerable<Product> products=new List<Product>();
+        List<Product> products=new List<Product>();
         try
         {
 
             products = (from p in productRoot.Elements()
                         select new Product()
                         {
-                            m_ID = Convert.ToInt32(p.Element("ID").Value),
-                            m_Name = p.Element("name")!.Value,
-                            m_InStock = Convert.ToInt32(p.Element("InStock").Value),
-                            m_Price = Convert.ToInt32(p.Element("Price").Value),
-                            m_Category = (DO.Enums.Category)Enum.Parse(typeof(DO.Enums.Category), (string)p.Element("Category")!)
+                            m_ID = Convert.ToInt32(p.Element("m_ID").Value),
+                            m_Name = p.Element("m_Name")!.Value,
+                            m_InStock = Convert.ToInt32(p.Element("m_InStock").Value),
+                            m_Price = Convert.ToInt32(p.Element("m_Price").Value),
+                            m_Category = (DO.Enums.Category)Enum.Parse(typeof(DO.Enums.Category), (string)p.Element("m_Category")!)
 
                         }).ToList();
 
@@ -116,7 +117,8 @@ internal class DalProduct : IProduct
         {
             products = null;
         }
-        return(IEnumerable<Product?>)products;
+        return products.Cast<DO.Product?>();
+    
     }
 
     public Product? GetSingle(Func<Product?, bool>? func)
@@ -128,11 +130,11 @@ internal class DalProduct : IProduct
             product = (from p in productRoot.Elements()
                        select new Product()
                        {
-                           m_ID = Convert.ToInt32(p.Element("ID").Value),
-                           m_Name = p.Element("Name")!.Value,
-                           m_InStock = Convert.ToInt32(p.Element("InStock").Value),
-                           m_Price = Convert.ToInt32(p.Element("Price").Value),
-                           m_Category = (DO.Enums.Category)Enum.Parse(typeof(DO.Enums.Category), (string)p.Element("Category")!)
+                           m_ID = Convert.ToInt32(p.Element("m_ID").Value),
+                           m_Name = p.Element("m_Name")!.Value,
+                           m_InStock = Convert.ToInt32(p.Element("m_InStock").Value),
+                           m_Price = Convert.ToInt32(p.Element("m_Price").Value),
+                           m_Category = (DO.Enums.Category)Enum.Parse(typeof(DO.Enums.Category), (string)p.Element("m_Category")!)
                        }).FirstOrDefault(p=>func(p)) ;
             produ = (Product?)product;
         }
@@ -151,15 +153,15 @@ internal class DalProduct : IProduct
         if (product == null)
             throw new NotExist();
         XElement studentElement = (from p in productRoot.Elements()
-                                   where Convert.ToInt32(p.Element("ID").Value) == P?.m_ID
+                                   where Convert.ToInt32(p.Element("m_ID").Value) == P?.m_ID
                                    select p).FirstOrDefault();
 
-        studentElement!.Element("Name")!.Value = P?.m_Name;
-        studentElement!.Element("Category")!.Value = P?.m_Category.ToString();
-        studentElement!.Element("InStock")!.Value = P?.m_InStock.ToString();
-        studentElement!.Element("Price")!.Value = P?.m_Price.ToString();
+        studentElement!.Element("m_Name")!.Value = P?.m_Name;
+        studentElement!.Element("m_Category")!.Value = P?.m_Category.ToString();
+        studentElement!.Element("m_InStock")!.Value = P?.m_InStock.ToString();
+        studentElement!.Element("m_Price")!.Value = P?.m_Price.ToString();
 
-        productRoot.Save(productPath);
+        productRoot.Save(dir + productPath);
     }
  
 }

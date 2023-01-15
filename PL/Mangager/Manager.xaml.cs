b4b40862e3f5fb31ffaaa68890d0672431e28c78
OchanaYael <yaochana@g.jct.ac.il>
2023-01-15 18:37:15 +0200

    ckvvv

--- PL/Mangager/Manager.xaml.cs
+++ PL/Mangager/Manager.xaml.cs
@@ -16,27 +16,29 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace PL
+namespace PL;
+
+/// <summary>
+/// Interaction logic for Manager.xaml
+/// </summary>
+public partial class Manager : Window
 {
-    /// <summary>
-    /// Interaction logic for Manager.xaml
-    /// </summary>
-    public partial class Manager : Window
+    Users user;
+    public Manager(Users u)
     {
-        Users user;
-        public Manager(Users u)
-        {
-            InitializeComponent();
-            user = u;
-            hello.Text = "Hello " + user.Name + "!";
-        }
-
-        private void ProductForList_Click(object sender, RoutedEventArgs e) { new Catalog(user).Show(); this.Close(); }
-
-        private void Click_buttonBack(object sender, RoutedEventArgs e) { new MainWindow().Show(); this.Close();}
-
-        private void OrderForList_Click(object sender, RoutedEventArgs e) { new OrderList(user).Show(); this.Close(); }
-
-        private void OrderTracking_Click(object sender, RoutedEventArgs e) { new OdrerTacking(user,4).Show(); this.Close(); }
+        InitializeComponent();
+        user = u;
+        hello.Text = "Hello " + user.Name + "!";
     }
+
+    private void ProductForList_Click(object sender, RoutedEventArgs e) { new Catalog(user).Show(); this.Close(); }
+
+    private void Click_buttonBack(object sender, RoutedEventArgs e) { new MainWindow().Show(); this.Close();}
+
+    private void OrderForList_Click(object sender, RoutedEventArgs e) { new OrderList(user).Show(); this.Close(); }
+
+    private void OrderTracking_Click(object sender, RoutedEventArgs e) { new OdrerTacking(user,4).Show(); this.Close(); }
+
+    private void OrdersTrack_Click(object sender, RoutedEventArgs e) { new OrdersTrack(user).Show(); this.Close(); }
+
 }
